6de6bf90d99a5db05fb38339262327c4d9418629
gerardfp <gerardfp@gmail.com>
2021-01-15 12:39:23 +0100

    cosmocalendar

--- app/src/main/java/com/example/eventsapp/ThirdFragment.java
+++ app/src/main/java/com/example/eventsapp/ThirdFragment.java
@@ -9,11 +9,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.eventsapp.databinding.FragmentThirdBinding;
+
+import java.util.Calendar;
+import java.util.HashSet;
+
 public class ThirdFragment extends Fragment {
 
+    private FragmentThirdBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_third, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentThirdBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.calendarView.setWeekendDays(new HashSet(){{
+            add(Calendar.SATURDAY);
+            add(Calendar.SUNDAY);
+        }});
     }
 }
